6ba002953d3183c242a7b1d24a7f6b5f2a40ac63
Alma Cane <almacane36@gmail.com>
2024-05-19 22:54:12 -0300

    token

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,6 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, userName, password } = req.body;
@@ -14,8 +14,10 @@ export const register = async (req, res) => {
     });
 
     const finalUser = await newUser.save();
+    const token = await createAccessToken({ id: finalUser._id });
+    res.cookie("token", token);
     res.json({
-      id: finalUser.id,
+      id: finalUser._id,
       userName: finalUser.userName,
       email: finalUser.email,
       createdAt: finalUser.createdAt,
