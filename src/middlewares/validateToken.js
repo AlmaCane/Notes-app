ad8f346c7808873065a146fa1fd7f88b11bc7a91
Alma Cane <almacane36@gmail.com>
2024-06-27 11:54:11 -0300

    update

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,3 +1,22 @@
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+import User from "../models/user.js";
+
 export const validateToken = (req, res, next) => {
-  res.send("validation");
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(403).json({ message: "Token not found" });
+  }
+
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(401 | { message: "Invalid token" });
+    }
+    req.user = user;
+    const userFound = User.findById(user.id);
+    if (!userFound) return res.status(404).json({ message: "User not found" });
+    return res.json(userFound);
+    next();
+  });
 };
