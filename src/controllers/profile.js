c4efbbdb756f442be1e0192fdca30c5c34cd8795
Alma Cane <almacane36@gmail.com>
2024-06-27 13:00:47 -0300

    update

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,3 +1,13 @@
+import User from "../models/user.js";
+
 export const profile = async (req, res) => {
-  res.send("profile");
+  const userFound = await User.findById(req.user.id);
+  if (!userFound) return res.status(400).json({ message: "User not found" });
+  return res.json({
+    id: userFound._id,
+    userName: userFound.userName,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
 };
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
-import User from "../models/user.js";
 
 export const validateToken = (req, res, next) => {
   const { token } = req.cookies;
@@ -14,9 +13,7 @@ export const validateToken = (req, res, next) => {
       return res.status(401 | { message: "Invalid token" });
     }
     req.user = user;
-    const userFound = User.findById(user.id);
-    if (!userFound) return res.status(404).json({ message: "User not found" });
-    return res.json(userFound);
+
     next();
   });
 };
